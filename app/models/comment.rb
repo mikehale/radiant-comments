0f745b41975df11160e5302b8e8ff7205c69b7d6
Michael Hale <mikehale@gmail.com>
2009-04-07 16:51:33 -0400

    use mikehale-akismet remove mollum code refactor comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
+require 'akismet'
+
 class Comment < ActiveRecord::Base
   belongs_to :page, :counter_cache => true
   validates_presence_of :author, :author_email, :content
   
   before_save :auto_approve
   before_save :apply_filter
-  after_save  :save_mollom_servers
-    
-  MOLLOM_SERVER_CACHE = RAILS_ROOT + '/tmp/mollom_servers.yaml'
     
   def self.per_page
     50
@@ -22,62 +21,17 @@ class Comment < ActiveRecord::Base
     @akismet ||= Akismet.new(Radiant::Config['comments.akismet_key'], Radiant::Config['comments.akismet_url'])
   end
   
-  def save_mollom_servers
-    if mollom.key_ok?
-      File.open(MOLLOM_SERVER_CACHE,'w') do |f|
-        f.write mollom.server_list.to_yaml
+  def auto_approve
+    if Radiant::Config['comments.auto_approve'] == "true" && akismet.verify?
+      result = akismet.ham?(akismet_attributes)
+      if result
+        self.approved_at = Time.now
+        RAILS_DEFAULT_LOGGER.debug("Comments: Auto-approving comment with akismet.")
       end
-    end
-  rescue Mollom::Error
-  end
-  
-  def mollom
-    return @mollom if @mollom
-    @mollom ||= Mollom.new(:private_key => Radiant::Config['comments.mollom_privatekey'], :public_key => Radiant::Config['comments.mollom_publickey'])
-    if (File.exists?(MOLLOM_SERVER_CACHE))
-      @mollom.server_list = YAML::load(File.read(MOLLOM_SERVER_CACHE))
-    end    
-    @mollom
-  end
-  
-  # If the Akismet details are valid, and Akismet thinks this is a non-spam
-  # comment, this method will return true
-  def auto_approve?
-    if akismet.valid?
-      # We do the negation because true means spam, false means ham
-      result = !akismet.commentCheck(
-        self.author_ip,            # remote IP
-        self.user_agent,           # user agent
-        self.referrer,             # http referer
-        self.page.url,             # permalink
-        'comment',                 # comment type
-        self.author,               # author name
-        self.author_email,         # author email
-        self.author_url,           # author url
-        self.content,              # comment text
-        {}                         # other
-      )
-      RAILS_DEFAULT_LOGGER.debug("Comments: Auto-approving comment with akismet.")
       result
-      elsif mollom.key_ok?
-        response = mollom.check_content(
-          :author_name => self.author,            # author name     
-          :author_mail => self.author_email,         # author email
-          :author_url => self.author_url,           # author url
-          :post_body => self.content              # comment text
-          )
-          ham = response.ham?
-          self.mollom_id = response.session_id
-       RAILS_DEFAULT_LOGGER.debug("Comments: Auto-approving comment with mollom.")
-       response.ham?
-    else
-      false
     end
-  rescue Mollom::Error
-    RAILS_DEFAULT_LOGGER.debug("Comments: Not auto-approving because a valid service is not configured.")
-    return false
   end
-  
+
   def unapproved?
     !approved?
   end
@@ -88,30 +42,25 @@ class Comment < ActiveRecord::Base
   
   def approve!
     self.update_attribute(:approved_at, Time.now)
+    akismet.submit_ham(akismet_attributes)
   end
-  
+
   def unapprove!
     self.update_attribute(:approved_at, nil)
-    # if we have to unapprove, and use mollom, it means
-    # the initial check was false. Submit this to mollom as Spam.
-    # Ideally, we'd need a different feedback for
-    #  - spam
-    #  - profanity
-    #  - unwanted
-    #  - low-quality
-     begin
-     if mollom.key_ok? and !self.mollom_id.empty?
-        mollom.send_feedback :session_id => self.mollom_id, :feedback => 'spam'
-      end
-    rescue Mollom::Error => e
-      raise Comment::AntispamWarning.new(e.to_s)
-    end
+    akismet.submit_spam(akismet_attributes)
   end
   
   private
   
-    def auto_approve
-      self.approved_at = Time.now if Radiant::Config['comments.auto_approve'] == "true" && auto_approve?
+    def akismet_attributes
+      {:user_ip => author_ip,
+      :referrer => referrer,
+      :permalink => page.url,
+      :comment_type => "comment",
+      :comment_author => author,
+      :comment_author_email => author_email,
+      :comment_author_url => author_url,
+      :comment_content => content}
     end
     
     def apply_filter
